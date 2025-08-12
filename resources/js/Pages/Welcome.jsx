2dc1d95fe8db4500f4ac76afc051d8c646ec1b1b
shubham-ynr <shubhamupadhyay96@icloud.com>
2025-08-13 01:00:48 +0530

    Final Adjustments

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,9 +1,12 @@
+import ThemeSwitcher from "@/components/theme-switcher";
+
 export default function Welcome() {
   console.log("Ziggy Route demo");
   console.log(route('Welcome'));
   return (
-    <div>
-      <h1>Welcome to the Dashboard</h1>
+    <div className="h-dvh w-dvw flex items-center justify-center text-center">
+      <h1 className="xl:text-4xl text-xl font-bold">Welcome to the Laravel React Template</h1>
+      <ThemeSwitcher className="absolute top-4 right-4" />
     </div>
   )
 }
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import './bootstrap';
 import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from './components/ui/theme-provider';
 
 createInertiaApp({
   resolve: name => {
@@ -9,7 +10,11 @@ createInertiaApp({
     return pages[`./Pages/${name}.jsx`]
   },
   setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
+    createRoot(el).render(
+      <ThemeProvider defaultTheme="system" storageKey="theme">
+        <App {...props} />
+      </ThemeProvider>
+    )
   },
   progress: {
     color: '#4B5563',
